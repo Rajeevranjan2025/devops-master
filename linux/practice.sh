faab5e746d45cf03de2e02c301d63233b8ddb195
Rajeevranjan2025 <rajeevranjanr225@gmail.com>
2026-05-14 21:35:38 +0000

    learned to read input

---
+++ linux/practice.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Bio Data"
+read -p "Enter your Name : " name
+read -p "Enter you Age : " age
+read -p "Enter your Company:  " company
+
+echo "===Details=="
+echo " "
+
+echo "Name :- $name"
+echo ""
+echo "Age :- $age"
+echo ""
+echo "Company Name :- $company"
